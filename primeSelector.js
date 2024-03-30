40bb53ccfeaea406a7b3a6fa65f662fdc3d49832
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-03-31 00:34:37 +0300

    Phase1-Week2-challenge

---
+++ primeSelector.js
@@ -0,0 +1,16 @@
+
+
+let number = [1,2,3,4,5,7,8,9,10]
+//function that Fiters and returns all prime numbers
+function primeInteger(arr){
+    let result = [];
+    for(value of arr){
+        //All prime numbers are not divisible by 2 and 3 hence should have a remainder
+        //Number 1 not prime number 
+        if((value % 2) !== 0 && (value % 3) !== 0 && value !== 1 || value === 3 || value === 2){
+            result.push(value)
+        }
+    }
+    return result;
+}
+console.log(primeInteger(number))
