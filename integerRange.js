4816c763fb7d00ff30bcd73ee52e2c735bed8267
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-04-01 08:44:39 +0300

    Phase1-Week2-challenge

--- integerRange.js
+++ integerRange.js
@@ -2,7 +2,7 @@
 function integerRange(startValue,endValue){
     //startVaue and endValue are the parameters of the range of numbers in the array.
     let array = [];
-    //Use for loop to recursively repeat value of i while incrementng its value.
+    //Use loop method to recursively repeat value of i while incrementng its value.
     for(let i = startValue;i <= endValue ;i++){
         array.push(i)
     }
